c35056f44aa334a5225fe7db24f4ef8ebd00475f
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-02-12 18:29:00 +0100

    Add constant data to be posted when creating new products

---
+++ data/items.js
@@ -0,0 +1,71 @@
+export const SOFA = {name: "Sofa", 
+                        articles:[
+                        {
+                            art_id: "1",
+                            amount_of: "4"
+                        },
+                        {
+                            art_id: "2",
+                            amount_of: "152"
+                        },
+                        {
+                            art_id: "3",
+                            amount_of: "2"
+                        },
+                        {
+                            art_id: "5",
+                            amount_of: "200"
+                        },
+                        {
+                            art_id: "6",
+                            amount_of: "200"
+                        }]
+                    }
+
+export const CHAIR = {name: "Dining Chair", 
+                        articles:[
+                        {
+                            art_id: "1",
+                            amount_of: "4"
+                        },
+                        {
+                            art_id: "2",
+                            amount_of: "30"
+                        },
+                        {
+                            art_id: "3",
+                            amount_of: "1"
+                        },
+                        {
+                            art_id: "5",
+                            amount_of: "15"
+                        },
+                        {
+                            art_id: "6",
+                            amount_of: "2"
+                        }]
+                    }
+
+export const TABLE = {name: "Dining Table", 
+                        articles:[
+                        {
+                            art_id: "1",
+                            amount_of: "4"
+                        },
+                        {
+                            art_id: "2",
+                            amount_of: "321"
+                        },
+                        {
+                            art_id: "4",
+                            amount_of: "1"
+                        },
+                        {
+                            art_id: "5",
+                            amount_of: "45"
+                        },
+                        {
+                            art_id: "6",
+                            amount_of: "4"
+                        }]
+                    }
